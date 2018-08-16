16f2ae51f16afe4750db1260f5c3a0cd69d67966
Tran Le <ttle@fullsail.edu>
2018-08-16 16:17:03 -0400

    Recreated layout file for landscape main activity

--- LeTran_CE05/app/src/main/java/com/sunny/android/letran_ce05/MainActivity.java
+++ LeTran_CE05/app/src/main/java/com/sunny/android/letran_ce05/MainActivity.java
@@ -7,16 +7,50 @@ package com.sunny.android.letran_ce05;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Person> dataCollection = new ArrayList<>();
+    Spinner dropdown = null;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dropdown = (Spinner)findViewById(R.id.spn_Spinner);
+        if (dropdown == null) {
+            Log.i(TAG, "Spinner is null!");
+        }
+
+//        getSupportFragmentManager().beginTransaction().add(R.id.personFragmentHolder,
+//                InstructorFragment.newInstance()).commit();
     }
+
+    private Spinner.OnItemSelectedListener spinnerTapped = new Spinner.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            switch (i) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    };
 }
